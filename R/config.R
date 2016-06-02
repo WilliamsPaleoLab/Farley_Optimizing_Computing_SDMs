0de54adda74dff6bf649683d419c57d3824cf045
Scott Farley <sfarley2@wisc.edu>
2016-06-02 13:56:26 -0500

    Update config.R

--- R/config.R
+++ R/config.R
@@ -1,5 +1,5 @@
 ## hidden database params
-username <- 'thesis-scripting'
-hostname  <- "107.180.50.243"
-dbname <- 'TimeSDM'
-password <- 'G0Bears7!'
+username <- '#'
+hostname  <- "#"
+dbname <- '#'
+password <- '##'
