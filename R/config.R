273993a18eff21ff4326cf7b3894ee8ae2ef6b5e
root <root@sdm-database-5.c.thesis-1329.internal>
2016-06-12 13:35:37 +0000

    Updated version of api from server

--- R/config.R
+++ R/config.R
@@ -1,5 +1,4 @@
-## hidden database params
-username <- '#'
-hostname  <- "#"
-dbname <- '#'
-password <- '##'
+username <- 'root'
+hostname <- "/cloudsql/thesis-1329:us-central1:sdm-database-3"
+password <- "G0Bears7!"
+dbname <- "timeSDM"
