80e9b2536f380b0541b4ae56d4c8eb0db190f237
scottsfarley93 <scott@firesphere.org>
2016-06-12 21:14:51 -0500

    Fixed

--- python/masterNode.py
+++ python/masterNode.py
@@ -7,8 +7,8 @@ def masterNode(iters):
     i = 0
     while i < iters:
         nextConfig = requests.get("http://104.154.235.236:8080/nextconfig").json()
-        cores = nextConfig['data']['cores']
-        memory = nextConfig['data']['GBMemory']
+        cores = nextConfig['data'][0]['cores']
+        memory = nextConfig['data'][0]['GBMemory']
         createAndManageGroup(compute, PROJECT, ZONE, cores, memory, 3)
         print "Finished group iteration #", i
 
