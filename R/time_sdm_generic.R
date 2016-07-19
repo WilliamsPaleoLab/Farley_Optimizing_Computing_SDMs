c4fa1d6e3aee7aec226468023033d3a9a64ae6b1
Scott Farley <sfarley2@wisc.edu>
2016-07-19 11:13:28 -0500

    modified monitor cpu script

---
+++ R/time_sdm_generic.R
@@ -0,0 +1,9 @@
+
+## load external libraries
+library(gbm) ## base pacakges for regression trees
+library(dismo) ## SDM package --> boosted regression tree function
+library(raster) ## for raster manipulation
+library(SDMTools) ## for accuracy assessment
+library(RMySQL) ## for database communication
+library(rgdal)
+library(parallel)
