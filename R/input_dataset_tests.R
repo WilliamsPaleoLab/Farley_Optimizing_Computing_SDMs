b3767ef290a8bc29e3d2cbb56ac6883259bf2835
Scott Farley <sfarley2@wisc.edu>
2016-09-07 17:10:20 -0500

    Update input_dataset_tests.R

--- R/input_dataset_tests.R
+++ R/input_dataset_tests.R
@@ -1,3 +1,4 @@
+install.packages(c("randomForest", "doMC", "foreach", "dismo", "raster", "gbm", "SDMTools", "RMySQL", "rgdal", "gam", "earth"), repos='http://cran.mtu.edu/')
 library(foreach)
 library(raster)
 library(dismo)
