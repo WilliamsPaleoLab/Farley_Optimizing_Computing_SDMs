7d8418962430b7e8bd22359d0f7c3d940aa937c1
Scott Farley <sfarley2@wisc.edu>
2016-09-15 15:03:45 -0500

    Changed predictor test script"

--- R/PredictorTests.R
+++ R/PredictorTests.R
@@ -1,4 +1,4 @@
-#install.packages(c("randomForest", "doMC", "foreach", "dismo", "raster", "gbm", "SDMTools", "RMySQL", "rgdal", "gam", "earth"), repos='http://cran.mtu.edu/')
+install.packages(c("randomForest", "doMC", "foreach", "dismo", "raster", "gbm", "SDMTools", "RMySQL", "rgdal", "gam", "earth"), repos='http://cran.mtu.edu/')
 library(foreach)
 library(raster)
 library(dismo)
@@ -8,7 +8,7 @@ library(randomForest)
 library(RMySQL)
 library(gbm)
 
-setwd("/users/scottsfarley/documents")
+setwd("/home/rstudio")
 
 source("thesis-scripts/R/config.R")
 
